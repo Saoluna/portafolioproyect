f23e565971f8b6330eabd902cc08b157d6008a95
Saoluna <seleneocantoluna@gmail>
2023-01-02 14:27:01 -0500

    Changes correcting sections

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,38 +1,27 @@
-import { useRef } from 'react'
 import './Contact.css'
 
-
 export const Contact =() =>{
-    const form =useRef();
-
-    const sendEmail= (e) =>{
-        e.preventDefault();
-        emailjs.senForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
-        .then((result) =>{
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    }
     return(
-        <div className="contact-form">
-            <div className="w-left">
-                <div className="span">
-                    <span>Ponerse en Contacto</span>
+         <div className="contact-form">
+             <div className="w-left">
+                <div className="awesome">
+                    <span>Ponerse en contácto</span>
+                    <br />
                     <span>Contáctame </span>
-                    <div className='blur'style={{background: "rgb(238 210 255)"}}></div>
+
+                    <div className='blur s-blur1'style={{background: "#ABF1FF94"}}></div>
                 </div>
+             </div>
 
                 <div className="c-rigth">
                     <form>
-                        <input type="text" name="user-name" className="user"  placeholder="Name" />
-                        <input type="email" name="user-email" className="user"  placeholder="Email"/>
-                        <textarea name="message" className="user" placeholder="Message" />
-                        <input type="submit" value="Send" className="button"/>
+                        <input type="text" name="user_name" className="user"  placeholder="Nombre" />
+                        <input type="email" name="user_email" className="user"  placeholder="Email"/>
+                        <textarea name="message" className="user" placeholder="Mensaje" />
+                        <input type="submit" value="Enviar" className="button"/>
                         <div className='blur'style={{background: "rgb(225, 146, 61)", top: '30rem', width: '100rem',height:'8rem', left: '11rem' }}></div>
                     </form>
                 </div>
-            </div>
-        </div>
-    )
-}
+         </div>
+      );
+};
